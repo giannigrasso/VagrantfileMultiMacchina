ec8b6783f90c2e14fc1821e8a876225606483b4e
Gianni Grasso <61271501+giannigrasso@users.noreply.github.com>
2022-09-21 12:14:02 +0200

    Delete Progetto4 directory

--- Progetto4/scripts/provision_php.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Php provisioning - begin"
-sudo apt-get install php libapache2-mod-php php-mysql -y
-sudo systemctl reload apache2
-echo "Php provisioning - end"
--- Progetto4/scripts/provision_update.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "APT provisioning - begin"
-sudo apt-get update -y
-echo "APT provisioning - end"
